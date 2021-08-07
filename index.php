a6641025df350a520657d8e4193d7209a5939618
Mohammad Haseeb Ashiq <learndotpk@gmail.com>
2021-08-07 01:05:09 -0700

    all project

---
+++ index.php
@@ -0,0 +1,393 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="./css/main.css">
+<script src="https://kit.fontawesome.com/07d7b7e553.js" crossorigin="anonymous"></script>    
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
+<title>Mufta Store</title>
+  </head>
+  <body>
+
+   <!-- nav bar -->
+
+   <nav class="navbar navbar-expand-sm navbar-dark bg-black">
+       <div class="container">
+           <a href="#" class="navbar-brand">Time Value</a>
+           <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarCollapse">
+               <ul class="navbar-nav ml-auto">
+                   <li class="nav-item active">
+                       <a href="#" class="nav-link">Home</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">
+                           Services
+                       </a>
+                   </li>
+                   <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        Products
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        About
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        Contact
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-shopping-cart fa-2x"></i>
+                    </a>
+                </li>
+               </ul>
+           </div>
+       </div>
+   </nav>
+   
+   <!-- nav bar end -->
+   
+<!-- slider sectoin -->
+
+<section id="main">
+    <div id="Carousel" class="carousel slide" data-ride="carousel">
+
+        <ol class="carousel-indicators">
+            <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#Carousel" data-slide-to="1"></li>
+            <li data-target="#Carousel" data-slide-to="2"></li>
+        </ol>
+<div class="carousel-inner">
+    <div class="carousel-item carousel-image-1 active">
+        <div class="container">
+            <div class="carousel-caption d-none d-sm-block text-right mb-5">
+               <h1 class="display-3 title-color">Title One</h1>
+               <p class="lead">Lorem ipsum, dolor sit
+                    amet consectetur adipisicing elit. Nemo, distinctio.</p>
+            <a href="#" class="btn btn-color slide-btn btn-lg">
+                Sign Up Now
+            </a>
+                </div>
+        </div>
+    </div>
+<!-- end first image -->
+    <div class="carousel-item carousel-image-2">
+        <div class="container">
+           <div class="carousel-caption d-none d-sm-block mb-5">
+               <h1 class="display-3 title-color">Title Two</h1>
+               <p class="lead">Lorem ipsum dolor sit amet consectetur, 
+                   adipisicing elit. Velit, rerum.</p>
+           <a href="#" class="btn btn-color slide-btn btn-lg">
+               Learn More
+           </a>
+                </div>
+        </div>
+    </div>
+<!-- end two image -->
+<div class="carousel-item carousel-image-3">
+    <div class="container">
+       <div class="carousel-caption d-none d-sm-block mb-5">
+           <h1 class="display-3 title-color">Title Three</h1>
+           <p class="lead">Lorem ipsum dolor sit amet consectetur, 
+               adipisicing elit. Velit, rerum.</p>
+       <a href="#" class="btn btn-color slide-btn btn-lg">
+           Learn More
+       </a>
+            </div>
+    </div>
+</div>
+<!-- end three image -->
+<a class="carousel-control-prev" href="#Carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#Carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </div>
+</section>
+
+<!-- end of slider section -->
+<!-- services section -->
+
+<section class="services py-5 text-center">
+    <div class="container">
+        <div class="row">
+            <!-- single service -->
+            <div class="col-10 mx-auto col-md-6 col-lg-4 my-3">
+                <span class="service-icon">
+                    <i class="fas fa-globe fa-2x"></i>
+                </span>
+                <h5 class="font-weight-bold text-uppercase">
+                    Worldwide Shipping
+                </h5>
+                <p class="text-capitalize">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, ut.
+                </p>
+            </div>
+            <!-- end of signle service -->
+
+             <!-- single service -->
+             <div class="col-10 mx-auto col-md-6 col-lg-4 my-3">
+                <span class="service-icon">
+                    <i class="fas fa-stamp fa-2x"></i>
+                </span>
+                <h5 class="font-weight-bold text-uppercase">
+                    Certified by Daraz.pk
+                </h5>
+                <p class="text-capitalize">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, ut.
+                </p>
+            </div>
+            <!-- end of signle service -->
+
+             <!-- single service -->
+             <div class="col-10 mx-auto col-md-6 col-lg-4 my-3">
+                <span class="service-icon">
+                    <i class="fas fa-file-invoice-dollar fa-2x"></i>
+                </span>
+                <h5 class="font-weight-bold text-uppercase">
+                    30 Days Money Back
+                </h5>
+                <p class="text-capitalize">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, ut.
+                </p>
+            </div>
+            <!-- end of signle service -->
+        </div>
+    </div>
+</section>
+
+<!-- end of services section -->
+    <!-- feature product section -->
+
+    <section id="products" class="products py-5">
+        <div class="container">
+            <div class="row">
+               <div class="col-10 mx-auto col-sm-6 text-center">
+                   <h1 class="text-capitalize product-title">
+                       Features Products
+                   </h1>
+               </div>
+            </div>
+            <!-- end product title -->
+            <!-- product items -->
+
+            <div class="row product-items" id="product-items">
+                <!-- single-item -->
+                <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3">
+                    <div class="card single-item">
+                        <div class="img-container">
+                            <img src="./images/watch-image-2.jpg" alt="" class="card-img-top product-img">
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text d-flex justify-content-between text-capitalize">
+                                 <h5 id="item-name">Branded Watch</h5>
+                                 <span><i class="fas fa-dollar-sign"></i> 350</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of single-item -->
+
+                <!-- single-item -->
+                <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3">
+                    <div class="card single-item">
+                        <div class="img-container">
+                            <img src="./images/hero-image-1.jpg" alt="" class="card-img-top product-img">
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text d-flex justify-content-between text-capitalize">
+                                 <h5 id="item-name">Sony Watch</h5>
+                                 <span><i class="fas fa-dollar-sign"></i> 220</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of single-item -->
+
+                <!-- single-item -->
+                <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3">
+                    <div class="card single-item">
+                        <div class="img-container">
+                            <img src="./images/background-watch.jpg" alt="" class="card-img-top product-img">
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text d-flex justify-content-between text-capitalize">
+                                 <h5 id="item-name">Xiami Watch</h5>
+                                 <span><i class="fas fa-dollar-sign"></i> 150</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of single-item -->
+
+                 <!-- single-item -->
+                 <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3">
+                    <div class="card single-item">
+                        <div class="img-container">
+                            <img src="./images/watch-image-2.jpg" alt="" class="card-img-top product-img">
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text d-flex justify-content-between text-capitalize">
+                                 <h5 id="item-name">Branded Watch</h5>
+                                 <span><i class="fas fa-dollar-sign"></i> 350</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of single-item -->
+
+                <!-- single-item -->
+                <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3">
+                    <div class="card single-item">
+                        <div class="img-container">
+                            <img src="./images/hero-image-1.jpg" alt="" class="card-img-top product-img">
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text d-flex justify-content-between text-capitalize">
+                                 <h5 id="item-name">Sony Watch</h5>
+                                 <span><i class="fas fa-dollar-sign"></i> 220</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of single-item -->
+
+                <!-- single-item -->
+                <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3">
+                    <div class="card single-item">
+                        <div class="img-container">
+                            <img src="./images/background-watch.jpg" alt="" class="card-img-top product-img">
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text d-flex justify-content-between text-capitalize">
+                                 <h5 id="item-name">Xiami Watch</h5>
+                                 <span><i class="fas fa-dollar-sign"></i> 150</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of single-item -->
+            </div>
+
+            <!-- end of product items -->
+        </div>
+    </section>
+
+    <!-- feature product section end -->
+    <!-- about section -->
+
+    <section id="about-sec">
+        <div class="container">
+            <div class="row align-item-center">
+               <div class="col-lg-5 text-center">
+                   <img src="./images/background-watch.jpg" width="450" height="150" alt=""
+                   class="img-fluid watch-img"
+                   />
+               </div>
+               <div class="col-lg-7 text-lg-right text-center text-color about-text">
+                   <h1>About Company</h1>
+                   <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae inventore omnis et recusandae voluptas dignissimos neque, vero,
+                        aliquam doloremque ab dolor nisi nihil,
+                         rerum aliquid?</p>
+                         
+                    </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section end -->
+    <!-- best seller section -->
+
+    <section class="seller py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-10 mx-auto col-sm-6 text-center">
+                    <h1 class="text-capitalize product-title">
+                        Best Seller Products
+                    </h1>
+                </div>
+            </div>
+            <!-- end of seller title -->
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="seller-item">
+                        <img src="./images/hero-image-1.jpg" alt="" class="img-fluid photo">
+                      <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <!-- end of first column -->
+                <div class="col-sm-6 d-flex flex-column justify-content-between">
+                    <!-- row -->
+                    <div class="row">
+                        <!-- img 1 -->
+                        <div class="col-sm-6">
+                            <div class="seller-item">
+                                <img src="./images/watch-image-2.jpg" alt="" class="img-fluid seller-img img-top">
+                                <p>Lorem ipsum dolor sit amet.</p>
+
+                            </div>
+                        </div>
+                        <!-- end of img 1 -->
+
+                                <!-- img 2 -->
+                                <div class="col-sm-6">
+                                    <div class="seller-item">
+                                        <img src="./images/hero-image-1.jpg" alt="" class="img-fluid seller-img img-top">
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                        
+                                    </div>
+                                </div>
+                                <!-- end of img 2 -->
+                    </div>
+                    <!-- end of row -->
+                    <div class="row">
+                         <!-- img 3 -->
+                         <div class="col-sm-6">
+                            <div class="seller-item">
+                                <img src="./images/background-watch.jpg" alt="" class="img-fluid seller-img img-top">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                
+                            </div>
+                        </div>
+                        <!-- end of img 3 -->
+                         <!-- img 4 -->
+                         <div class="col-sm-6">
+                            <div class="seller-item">
+                                <img src="./images/hero-image-1.jpg" alt="" class="img-fluid seller-img img-top">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                
+                            </div>
+                        </div>
+                        <!-- end of img 4 -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end of best seller section -->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
+  </body>
+</html>
